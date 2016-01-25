17b2975b4a743452956355a44059568737804d59
Abel de Beer <abel@cookingfox.nl>
2016-01-25 14:29:27 +0100

    Updates Java version and supported library versions

--- src/main/java/com/cookingfox/eventbus/adapter/GuavaEventBusAdapter.java
+++ src/main/java/com/cookingfox/eventbus/adapter/GuavaEventBusAdapter.java
@@ -16,14 +16,17 @@ public class GuavaEventBusAdapter implements EventBus {
         this.eventBus = eventBus;
     }
 
+    @Override
     public void post(Object event) {
         eventBus.post(event);
     }
 
+    @Override
     public void register(Object subscriber) {
         eventBus.register(subscriber);
     }
 
+    @Override
     public void unregister(Object subscriber) {
         eventBus.unregister(subscriber);
     }
